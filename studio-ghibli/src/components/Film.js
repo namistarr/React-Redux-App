5226c624df1a639b72e3e2a29ae68e6504c8266c
namistarr <namistarr@icloud.com>
2019-08-15 15:42:16 -0700

    added styled-components dependency

--- studio-ghibli/src/components/Film.js
+++ studio-ghibli/src/components/Film.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Film = props => {
     return (
         <div>
-            <h3>{props.film.title}</h3>
+            <h3>{props.film.title}, {props.film.release_date}</h3>
+            <p>Director: {props.film.director}</p>
             <p>{props.film.description}</p>
         </div>
     )
