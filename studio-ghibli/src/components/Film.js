fecb7969e495b4e55569aba433ece98620f7d73e
namistarr <namistarr@icloud.com>
2019-08-15 16:01:07 -0700

    adding styles

--- studio-ghibli/src/components/Film.js
+++ studio-ghibli/src/components/Film.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FilmTitle = styled.h2`
+font-size: 1.8rem;
+`
+const Director = styled.p`
+font-style: italic
+`
+
 
 const Film = props => {
     return (
         <div>
-            <h3>{props.film.title}, {props.film.release_date}</h3>
-            <p>Director: {props.film.director}</p>
+            <FilmTitle>{props.film.title}, {props.film.release_date}</FilmTitle>
+            <Director>Director: {props.film.director}</Director>
             <p>{props.film.description}</p>
         </div>
     )
--- studio-ghibli/src/components/FilmList.js
+++ studio-ghibli/src/components/FilmList.js
@@ -2,20 +2,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { getFilms } from '../actions';
 import Loader from 'react-loader-spinner';
+import styled from 'styled-components';
 
 import Film from './Film';
+
+const Button = styled.button`
+background: none;
+border: 1px solid white;
+border-radius: 8px;
+color: white;
+font-size: 1rem;
+width: 100px;
+`
+
+
 
 const FilmList = props => {
     return (
         <div className='film-container'>
-            <h1>Welcome</h1>
-            <button className ='film-button' onClick={props.getFilms}>
+            <h1>Studio Ghibli</h1>
+            <Button className ='film-button' onClick={props.getFilms}>
                 {props.isLoading ? (
-                    <Loader type='Oval' color='#FFA500' height={15} width={100} />
+                    <Loader type='Oval' color='#FFFFFF' height={15} width={100} />
                 ) : (
-                    'See Studio Ghibli Films'
+                    'See Films'
                 )}
-            </button>
+            </Button>
             {props.films && 
                 props.films.map(film => <Film key={film.title} film={film} />)}
         </div>
